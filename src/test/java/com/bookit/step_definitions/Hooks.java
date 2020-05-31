8475ed1dfec642cc84f074c0e79253f49c8b5f21
bulutQ <58412815+bulutQ@users.noreply.github.com>
2020-05-31 09:45:56 -0500

    updated all

---
+++ src/test/java/com/bookit/step_definitions/Hooks.java
@@ -0,0 +1,22 @@
+package com.bookit.step_definitions;
+
+import com.bookit.utilities.DBUtility;
+import com.bookit.utilities.Environment;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    /**
+     * This hook will be executed only for scenarios that are annotated with @db
+     */
+    @Before("@db")
+    public void dbSetup(){
+        DBUtility.createDBConnection(Environment.DB_HOST,Environment.DB_USERNAME,Environment.DB_PASSWORD);
+    }
+
+    @After("@db")
+    public void dbTearDown(){
+        DBUtility.destroy();
+    }
+}
